fbd6533407674beff6a3858ad85efeef62ce0f1c
Bibash Rajthala <bibashrajthala00@gmail.com>
2022-11-24 17:01:37 +0545

    completed the header seciton

---
+++ src/assets/index.js
@@ -0,0 +1,17 @@
+import headerImg from "./header_image.png";
+import headerSide1 from "./header_side1.png";
+import headerSide2 from "./header_side2.png";
+import headerSide3 from "./header_side3.png";
+import headerCheck from "./header_check.png";
+import headerBtn from "./header_btn.png";
+
+const assets = {
+  headerImg,
+  headerSide1,
+  headerSide2,
+  headerSide3,
+  headerCheck,
+  headerBtn,
+};
+
+export default assets;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,60 @@
-import React from 'react'
-import './header.scss'
+import React from "react";
+import "./header.scss";
+
+import headerImages from "../../assets";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="header">
+      <div className="header__content">
+        <h2 className="header__text--main">
+          Managing freelance
+          <br /> payments has never <br />
+          been easier
+        </h2>
 
-export default Header
+        <p className="header__text">
+          Yet bed any for travelling assistance indulgence <br />
+          unpleasing. Not thoughts all exercise blessing.{" "}
+        </p>
+
+        <div className="header__btns">
+          <button className="header__btns-btn1">Get Started</button>
+          <div className="header__btns-btn2">
+            <div className="header__btns-btn2-polygon">
+              <img src={headerImages.headerBtn} alt="headerBtn" />
+            </div>
+            <span>See How It Works</span>
+          </div>
+        </div>
+
+        <div className="header__checks">
+          <div className="header__check">
+            <img src={headerImages.headerCheck} alt="headerCheck" />
+            <span>Free Register</span>
+          </div>
+          <div className="header__check">
+            <img src={headerImages.headerCheck} alt="headerCheck" />
+            <span>Great Service</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="header__image">
+        <img src={headerImages.headerImg} alt="headerImg" />
+      </div>
+
+      <div className="header__side header__side--1">
+        <img src={headerImages.headerSide1} alt="headerSide1s" />
+      </div>
+      <div className="header__side header__side--2">
+        <img src={headerImages.headerSide2} alt="headerSide2" />
+      </div>
+      <div className="header__side header__side--3">
+        <img src={headerImages.headerSide3} alt="headerSide3" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
